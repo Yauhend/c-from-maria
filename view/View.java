a4ea5f632265c46d8e2105d6cc3e74bbb9dae14c
Yauhend <30414218+Yauhend@users.noreply.github.com>
2018-02-23 11:57:42 +0300

    Add files via upload

---
+++ view/View.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package by.epam.pretraining.task6.view;
+
+/**
+ *
+ * @author Student
+ */
+
+public class View {
+
+    public static void printToConsole(Object toPrint) {
+        System.out.println(toPrint);
+    }
+}
